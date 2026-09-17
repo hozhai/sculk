c943a3133bba98beeef9ad7fead2b3f59aa96fd2
hozhai <zhaihongmeng@gmail.com>
2026-09-17 16:05:53 -0300

    feat: automatically limbo bot

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,22 @@
-import { createEvent } from 'seyfert';
+import { getMinecraft } from "@bridge/load.js";
+import { createEvent } from "seyfert";
 
 export default createEvent({
-  // botReady is triggered when all shards and servers are ready.
-  // `once` ensures the event runs only once.
-  data: { once: true, name: 'botReady' },
+  data: { once: true, name: "botReady" },
   run(user, client) {
+    client.logger.info(
+      `${user.username}#${user.discriminator} (${user.id}) is ready`
+    );
 
-    //  We can use client.logger to display messages in the console.
-    client.logger.info(`${user.username}#${user.discriminator} (${user.id}) is ready`);
-  }
-})
+    // load mineflayer
+    getMinecraft();
+
+    // we HAVE to call getMinecraft() every time
+    // otherwise we get a bunch of errors :)
+    getMinecraft().on("spawn", () => {
+      getMinecraft().chat("/limbo");
+    });
+
+    client.logger.info(`Succesfully connected and executed /limbo`);
+  },
+});
