0d65cb9b5da4681163b2d1e7e8489e3fcaaca32e
hozhai <zhaihongmeng@gmail.com>
2026-09-18 22:06:27 -0300

    fix(events): consider config for replaceLink and replaceAttachment functionalities

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -38,12 +38,16 @@ export default createEvent({
       messageContent = messageContent.replaceAll("discord", "disc0rd");
     }
 
-    const urlPattern =
-      /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
-    messageContent = messageContent.replace(urlPattern, "[link]");
+    if (config.bridge.replaceLink) {
+      const urlPattern =
+        /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+      messageContent = messageContent.replace(urlPattern, "[link]");
+    }
 
-    if (message.attachments.length > 0) {
-      messageContent += "[attachment(s)]";
+    if (config.bridge.replaceAttachment) {
+      if (message.attachments.length > 0) {
+        messageContent += "[attachment(s)]";
+      }
     }
 
     const nonMessageLength =
